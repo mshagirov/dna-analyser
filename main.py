2ae6dc19c32fb2468fae647fcee7e84ef30131d2
mshagirov <muratshagirov@gmail.com>
2025-11-07 16:54:05 +0800

    tasks for the project

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from banner import banner
 
 def main():
     print(banner)
-
     print("Hello there!")
 
-
+    # check if it is DNA
+    # length
+    # weight?
+    # GC content
+    # complementary strand
+    # mRNA seq
 
 if __name__ == "__main__":
     main()
